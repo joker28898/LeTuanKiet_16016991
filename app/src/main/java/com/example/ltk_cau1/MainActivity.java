3a2c8b943757f6c9a2171a0c7c0f853aed6b4b87
kiet015987 <kiet015987@gmail.com>
2019-09-10 22:51:07 +0700

    add button "Thoát"

--- app/src/main/java/com/example/ltk_cau1/MainActivity.java
+++ app/src/main/java/com/example/ltk_cau1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ltk_cau1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +12,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mButtonDangNhap;
+    private Button mButtonDangNhap, mButtonThoat;
     private CheckBox mCheckBox;
 
     @Override
@@ -29,10 +31,39 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        mButtonThoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Dialog();
+            }
+        });
     }
 
     private void initView() {
         mButtonDangNhap = findViewById(R.id.button_DangNhap);
         mCheckBox = findViewById(R.id.checkBox_LuuTT);
+        mButtonThoat = findViewById(R.id.button_Thoat);
+    }
+    protected void Dialog(){
+        AlertDialog.Builder ad =  new AlertDialog.Builder(this);
+        ad.setTitle("Thông báo");
+        ad.setMessage("Bạn muốn thoát không ?");
+        ad.setPositiveButton("không", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        ad.setNegativeButton("có", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        ad.show();
+
     }
 }
